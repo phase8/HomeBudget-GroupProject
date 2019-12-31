52889024b2d0baf9583a7a142a410b52a87c55b5
PSJIM <stolarz.p@gmail.com>
2019-12-31 16:16:58 +0100

    1/2 way commit

--- client/src/Router.js
+++ client/src/Router.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import Signup from './pages/Signup';
 import Signin from './pages/Signin';
+import Welcomepage from './pages/Welcomepage'
 
 const Router = () => {
   return (
     <Switch>
       <Route exact path='/' component={Signin}></Route>
       <Route exact path='/signup' component={Signup}></Route>
+      <Route exact path='/Welcomepage' component={Welcomepage}></Route>
     </Switch>
   );
 }
---
+++ client/src/pages/Welcomepage.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import '../styles/welcomepage.css'
+
+class Welcomepage extends React.Component{
+    render() {
+        return (
+            <div className='pageContainer'>
+                <div className='controls'>
+                    <div className='controlButton'>
+                        Panel użytkownika
+                    </div>
+                    <div className='controlButton'>
+                        Cele
+                    </div>
+                    <div className='controlButton'>
+                        Kategorie
+                    </div>
+                    <div className='controlButton'>
+                        Historia
+                    </div>
+                    <Link to ='/'>
+                        <div className='controlButton'>
+                            Wyloguj (nazwa użytkownika)
+                        </div>
+                    </Link>
+                </div>
+                <div className='welcomeAgain'>
+                    Cześć (nazwa użytkownika)! Miło Cię widzieć ponownie. 
+                    Zaoszczędź z nami trochę $$$ żeby Ci potem starczyło na waciki. 
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default Welcomepage;
